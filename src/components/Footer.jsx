25871edb7892c2572824b93f907d3524676e66f7
Kjubism <dev@hellothere.de>
2023-08-05 16:41:18 +0200

    add links and favicons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,13 +2,22 @@ function Footer() {
 	return (
 		<>
 			<footer className="footer">
-				<a
-					href="https://www.hellothere.de"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					By <strong>HelloThere.</strong>
-				</a>
+				<nav>
+					<ul>
+						<li>
+							<a
+								href="https://www.hellothere.de"
+								target="_blank"
+								rel="noopener noreferrer"
+							>
+								By <strong>HelloThere.</strong>
+							</a>
+						</li>
+						<li>
+							<a href="/imprint">Imprint</a>
+						</li>
+					</ul>
+				</nav>
 			</footer>
 		</>
 	);
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,24 @@
 import { Cooking } from '@icon-park/react';
 
 function Header() {
-  return (
-    <>
-      <header className="header">
-        <Cooking size={'1.75em'} theme="filled" />
-        <span className="header__overline">Mum, tell me…</span>
-        <h1 className="header__title">What's on the Plate</h1>
-      </header>
-      <div className="o-line"></div>
-    </>
-  );
+	return (
+		<>
+			<header className="header">
+				<a
+					href="/"
+					aria-label="Back to Swiping"
+				>
+					<Cooking
+						size={'1.75em'}
+						theme="filled"
+					/>
+				</a>
+				<span className="header__overline">Mum, tell me…</span>
+				<h1 className="header__title">What's on the Plate</h1>
+			</header>
+			<div className="o-line"></div>
+		</>
+	);
 }
 
 export default Header;
